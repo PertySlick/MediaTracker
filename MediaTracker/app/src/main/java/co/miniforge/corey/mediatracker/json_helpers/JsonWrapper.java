f8d1797cf7d0081d5f014fb59816e14d83fb3506
Tim Roush <perty_slick@hotmail.com>
2017-10-29 21:41:17 -0700

    Assignment 4 Post-Cleanup - JsonWrapper.java JavaDocs and code cleanup.

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/json_helpers/JsonWrapper.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/json_helpers/JsonWrapper.java
@@ -3,30 +3,44 @@ package co.miniforge.corey.mediatracker.json_helpers;
 import org.json.JSONObject;
 
 /**
- * Created by Perty on 10/26/2017.
+ * Utility class for providing reusable methods for JSON Object related operations including get()
+ * and put();
+ *
+ * @author Timothy Roush
+ * @version 1.0
  */
 
 public class JsonWrapper {
 
-    // Get value from json Object
+    /**
+     * Returns the requested object from the specified JSON Object.
+     * @param tag String property of the JSON Object to retrieve
+     * @param jsonObject JSONObject JSON object to retrieve from
+     * @return Object value stored for specified tag
+     */
     public static Object get(String tag, JSONObject jsonObject) {
         try {
             return jsonObject.get(tag);
         } catch (Exception e) {
             JSONExceptionLogger.logError(e);
         }
-
         return null;
     }
 
-    // Put value into json object with tag
+    /**
+     * Places the specified Object into the provided JSONObject within the supplied tag value.
+     * @param tag String property to associate Object with
+     * @param o Object value or object to be placed within JSONObject
+     * @param jsonObject JSONObject JSON object to place value in
+     * @return true if successful, false otherwise
+     */
     public static boolean put(String tag, Object o, JSONObject jsonObject) {
         try {
             jsonObject.put(tag, o);
+            return true;
         } catch (Exception e) {
             JSONExceptionLogger.logError(e);
         }
-
         return false;
     }
 }
