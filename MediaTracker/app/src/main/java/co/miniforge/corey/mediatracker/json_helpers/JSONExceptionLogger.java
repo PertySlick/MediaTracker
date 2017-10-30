8c1eea577b5b74391474f8d69bf6f13ce48278d1
Tim Roush <perty_slick@hotmail.com>
2017-10-29 21:34:30 -0700

    Assignment 4 Post-Cleanup - JSONExceptionLogger.java JavaDocs and code cleanup.

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/json_helpers/JSONExceptionLogger.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/json_helpers/JSONExceptionLogger.java
@@ -3,16 +3,19 @@ package co.miniforge.corey.mediatracker.json_helpers;
 import android.util.Log;
 
 /**
- * Created by Perty on 10/26/2017.
+ * Utility class for providing a reusable exception logging method for JSON Object related errors
+ *
+ * @author Timothy Roush
+ * @version 1.0
  */
 
 public class JSONExceptionLogger {
-
+    /**
+     * Outputs an error message template to the debug log that includes the provided error message
+     * @param e Error object provided by an Exception
+     */
     public static void logError(Exception e) {
         Log.e("JsonError", "There wa an error that occured: \n" + e.getMessage());
         e.printStackTrace();
     }
-
-
-
 }
